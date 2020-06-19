23c6dbf0079c212671c60daea4f4d40b63ff7216
Jay Brodeur <jason.brodeur@gmail.com>
2020-06-19 16:26:52 -0400

    modified

--- timelapse.py
+++ timelapse.py
@@ -5,9 +5,8 @@ camera = PiCamera()
 
 camera.resolution = (1920, 1080)
 camera.start_preview()
-i = 0
 
-for i in range(40000):
-    sleep(5)    
-    camera.capture('/home/pi/Pictures/timelapse/image_%s.jpg' % i)
+for i in range(1,40000):
+    sleep(10)    
+    camera.capture('/home/pi/Pictures/lego-timelapse/image_%s.jpg' % i)
 camera.stop_preview()
